7091ce3148dca3915b634314654a5b6b7904f52e
Edward Graham <Edward@Edwards-MBP.fios-router.home>
2017-07-20 19:04:53 -0400

    Refactored album page to use angular and create album controller.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,12 +12,13 @@
       })
       .state('album', {
         url: '/album',
+        controller: 'AlbumCtrl as album',
         templateUrl: '/templates/album.html'
       })
       .state('collection', {
         url: '/collection',
         controller: 'CollectionCtrl as collection',
-        templateUrl: '/templates/index.html'
+        templateUrl: '/templates/collection.html'
       });
   }
   angular
