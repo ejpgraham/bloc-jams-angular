99b4add6c61f877bb3e8ae13a854a987100e0b88
Edward Graham <Edward@Edwards-MBP.fios-router.home>
2017-07-25 16:29:03 -0400

    Add stopSong(), SongPlayer.next, and artist/song info

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -13,8 +13,7 @@
 
     var setSong = function(song){
       if (currentBuzzObject) {
-        currentBuzzObject.stop();
-        SongPlayer.currentSong.playing = null;
+        stopSong();
       }
       /**
       * @desc Buzz object audio file
@@ -34,6 +33,11 @@
 * @param {Object} song
 */
 
+  var stopSong = function(){
+    currentBuzzObject.stop();
+    SongPlayer.currentSong.playing = null;
+  };
+
   var playSong = function(song){
     currentBuzzObject.play();
     song.playing = true;
@@ -52,6 +56,7 @@
   * @type {Object}
   */
   SongPlayer.currentSong = null;
+  SongPlayer.currentAlbum = Fixtures.getAlbum();
 
   /**
 * @function SongPlayer.play
@@ -94,13 +99,24 @@
     currentSongIndex--;
 
     if (currentSongIndex < 0){
-      currentBuzzObject.stop();
-      SongPlayer.currentSong.playing = null;
+      stopSong();
     } else {
       var song = currentAlbum.songs[currentSongIndex];
       setSong(song);
       playSong(song);
     }
+  };
+
+  SongPlayer.next = function() {
+    var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+    currentSongIndex++;
+
+    if (currentSongIndex >= currentAlbum.songs.length){
+      currentSongIndex = 0;
+    }
+    var song = currentAlbum.songs[currentSongIndex];
+    setSong(song);
+    playSong(song);
   };
 
     return SongPlayer;
